fc6da3accd446a7f9901bd1ee6aedc5df940824d
huangchengkang <huangchengkang@lattebank.com>
2022-06-22 20:13:55 +0800

    0.0.2

---
+++ validation-plus-spring-boot-starter/src/test/java/com/github/kancyframework/validationplus/validator/AssertConstraintValidatorTests.java
@@ -0,0 +1,18 @@
+package com.github.kancyframework.validationplus.validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * AssertConstraintValidatorTests
+ *
+ * @author huangchengkang
+ * @date 2022/6/22 20:06
+ */
+public class AssertConstraintValidatorTests {
+
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
